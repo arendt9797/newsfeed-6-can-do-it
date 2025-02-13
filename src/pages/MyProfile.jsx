93f8c8bf5009bc96f790e86185f6fbeec8ffb83a
Hyojin-Moon <moontough8@gmail.com>
2025-02-13 15:48:54 +0900

    📝 Docs: 기본 폼 생성

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,5 +1,16 @@
 function MyProfile() {
-  return <div>MyProfile</div>;
+  return <div>
+    <h2>My Profile</h2>
+    <form>
+      <label>아이디</label>
+      <label>비밀번호</label>
+      <label>E-mail</label>
+      <label>GitHub</label>
+      <label>Vlog</label>
+      <label>관심사?</label>
+      <button>수정완료</button>
+    </form>
+  </div>;
 }
 
 export default MyProfile;
