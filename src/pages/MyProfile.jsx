5438596bed6669f85081325ff6d93e10bd7bac42
Hyojin-Moon <moontough8@gmail.com>
2025-02-13 16:24:05 +0900

    ✨ Feat: 프로필 state 추가 및 인풋추가

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 function MyProfile() {
+
+  const [profile, setProfile] = useState({
+    image:"",
+    userId:"",
+    password:"",
+    email:"",
+    github:"",
+    blog:"",
+  })
+
   return <div>
     <h2>My Profile</h2>
     <form>
+      <label>이미지</label>
+      <input type="file" value={profile.image}/>
       <label>아이디</label>
+      <input type="text" value={profile.userId}/>
       <label>비밀번호</label>
+      <input type="text" value={profile.password}/>
       <label>E-mail</label>
+      <input type="text" value={profile.email}/>
       <label>GitHub</label>
-      <label>Vlog</label>
+      <input type="url" value={profile.github}/>
+      <label>Blog</label>
+      <input type="url" value={profile.blog}/>
       <label>관심사?</label>
       <button>수정완료</button>
     </form>
