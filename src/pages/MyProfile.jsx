aaef4c5b4ea646a15034a0b1b304e51058c2c3f7
Hyojin-Moon <moontough8@gmail.com>
2025-02-13 16:40:25 +0900

    ✨ Feat: onchange 함수 임시 셋팅

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -3,29 +3,41 @@ import { useState } from "react";
 function MyProfile() {
 
   const [profile, setProfile] = useState({
-    image:"",
-    userId:"",
-    password:"",
-    email:"",
-    github:"",
-    blog:"",
-  })
+    image: "",
+    userId: "test",
+    password: "test",
+    email: "test",
+    github: "test",
+    blog: "test",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventdefault();
+    // supabase를 통해 프로필 업데이트 하는 로직
+  };
 
   return <div>
     <h2>My Profile</h2>
-    <form>
+    <form onChange={handleSubmit}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center"
+      }}
+    >
       <label>이미지</label>
-      <input type="file" value={profile.image}/>
+      <input type="file" value={profile.image} />
       <label>아이디</label>
-      <input type="text" value={profile.userId}/>
+      <input type="text" value={profile.userId} />
       <label>비밀번호</label>
-      <input type="text" value={profile.password}/>
+      <input type="text" value={profile.password} />
       <label>E-mail</label>
-      <input type="text" value={profile.email}/>
+      <input type="text" value={profile.email} />
       <label>GitHub</label>
-      <input type="url" value={profile.github}/>
+      <input type="url" value={profile.github} />
       <label>Blog</label>
-      <input type="url" value={profile.blog}/>
+      <input type="url" value={profile.blog} />
       <label>관심사?</label>
       <button>수정완료</button>
     </form>
