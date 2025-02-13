8373b11f8c1a6f36aaf876d7b9261ac2401cd479
Hyojin-Moon <moontough8@gmail.com>
2025-02-13 16:50:30 +0900

    ✨ Feat: onchange 임시 셋팅, onSubmit 오타수정, preventDefault 오타수정

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -12,13 +12,17 @@ function MyProfile() {
   });
 
   const handleSubmit = async (e) => {
-    e.preventdefault();
+    e.preventDefault();
     // supabase를 통해 프로필 업데이트 하는 로직
+  };
+
+  const handleChange = () => {
+
   };
 
   return <div>
     <h2>My Profile</h2>
-    <form onChange={handleSubmit}
+    <form onSubmit={handleSubmit}
       style={{
         display: "flex",
         alignItems: "center",
@@ -27,18 +31,25 @@ function MyProfile() {
       }}
     >
       <label>이미지</label>
-      <input type="file" value={profile.image} />
+      <input type="file" value={profile.image} onChange={handleChange} />
+
       <label>아이디</label>
-      <input type="text" value={profile.userId} />
+      <input type="text" value={profile.userId} onChange={handleChange} />
+
       <label>비밀번호</label>
-      <input type="text" value={profile.password} />
+      <input type="text" value={profile.password} onChange={handleChange} />
+
       <label>E-mail</label>
-      <input type="text" value={profile.email} />
+      <input type="text" value={profile.email} onChange={handleChange} />
+
       <label>GitHub</label>
-      <input type="url" value={profile.github} />
+      <input type="url" value={profile.github} onChange={handleChange} />
+
       <label>Blog</label>
-      <input type="url" value={profile.blog} />
+      <input type="url" value={profile.blog} onChange={handleChange} />
+
       <label>관심사?</label>
+
       <button>수정완료</button>
     </form>
   </div>;
