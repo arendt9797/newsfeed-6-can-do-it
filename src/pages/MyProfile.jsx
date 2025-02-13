8c9af95449634f68e55caa9d6698f48720d945b0
Hyojin-Moon <moontough8@gmail.com>
2025-02-13 17:56:25 +0900

    💄 Style: styled-components로 UI 디자인 수정

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 function MyProfile() {
 
@@ -20,39 +21,77 @@ function MyProfile() {
 
   };
 
-  return <div>
-    <h2>My Profile</h2>
-    <form onSubmit={handleSubmit}
-      style={{
-        display: "flex",
-        alignItems: "center",
-        flexDirection: "column",
-        justifyContent: "center"
-      }}
-    >
-      <label>이미지</label>
-      <input type="file" value={profile.image} onChange={handleChange} />
+  return (
+    <StProfileContainer>
+      <h2>My Profile</h2>
+      <StForm onSubmit={handleSubmit} >
+        <label>이미지</label>
+        <StInput type="file" value={profile.image} onChange={handleChange} />
 
-      <label>아이디</label>
-      <input type="text" value={profile.userId} onChange={handleChange} />
+        <label>아이디</label>
+        <StInput type="text" value={profile.userId} onChange={handleChange} />
 
-      <label>비밀번호</label>
-      <input type="text" value={profile.password} onChange={handleChange} />
+        <label>비밀번호</label>
+        <StInput type="text" value={profile.password} onChange={handleChange} />
 
-      <label>E-mail</label>
-      <input type="text" value={profile.email} onChange={handleChange} />
+        <label>E-mail</label>
+        <StInput type="text" value={profile.email} onChange={handleChange} />
 
-      <label>GitHub</label>
-      <input type="url" value={profile.github} onChange={handleChange} />
+        <label>GitHub</label>
+        <StInput type="url" value={profile.github} onChange={handleChange} />
 
-      <label>Blog</label>
-      <input type="url" value={profile.blog} onChange={handleChange} />
+        <label>Blog</label>
+        <StInput type="url" value={profile.blog} onChange={handleChange} />
 
-      <label>관심사?</label>
+        <label>관심사?</label>
 
-      <button>수정완료</button>
-    </form>
-  </div>;
+        <StButton>수정완료</StButton>
+      </StForm>
+    </StProfileContainer>
+  );
 }
 
 export default MyProfile;
+
+const StProfileContainer = styled.div`
+  width: 400px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  padding: 2rem;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+  background: #fff;
+`;
+
+const StForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 1rem;
+`;
+
+const StInput = styled.input`
+  width: 100%;
+  padding: 0.5rem;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  font-size: 1rem;
+`;
+
+const StButton = styled.button`
+  background: #201e1e;
+  color: white;
+  padding: 0.7rem;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 1rem;
+
+  &:hover {
+    background: #6b5f60;
+  }
+`;
