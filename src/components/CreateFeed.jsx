ef07f56de61f62f86d619e15e5530ee9e9a16586
bluepome98 <wpdn1998@gmail.com>
2025-02-15 00:32:19 +0900

    🩹 fix 테이블 참조시 auth 스키마가 아닌 public 스키마에서 외래키를 통해 참조할 것, 현재는 주석 처리

--- src/components/CreateFeed.jsx
+++ src/components/CreateFeed.jsx
@@ -76,21 +76,21 @@ const CreateFeed = () => {
   const handleAddFeed = async () => {
     console.log('handleAddFeed 호출됨');
 
-    const {
-      data: { user },
-      error: authError,
-    } = await supabase.auth.getUser();
-    //현재 로그인 한 사용자의 정보 불러오기
-    //2025.02.14 기준 로그아웃 상태에서 콘솔에서 에러 확인
-    if (authError) {
-      console.log('auth에러', authError);
-    }
+    // const {
+    //   data: { users },
+    //   error: authError,
+    // } = await supabase.auth.getUser();
+    // //현재 로그인 한 사용자의 정보 불러오기
+    // //2025.02.14 기준 로그아웃 상태에서 콘솔에서 에러 확인
+    // if (authError) {
+    //   console.log('auth에러', authError);
+    // }
 
     try {
       const { data, error } = await supabase
         .from('feeds')
-        .insert([{ title, content, user_id: user.id }]);
-      //user_id라는 수파베이스 데이터 칼럼에 현재 user.id를 넣기
+        .insert([{ title, content}]);
+      //user_id라는 수파베이스 데이터 칼럼에 현재 user.id를 넣기 =>user_id: users.id 
       if (error) {
         console.log('error=>', error);
       } else {
