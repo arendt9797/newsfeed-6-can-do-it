9153fd8cd4b52a0a88d1dfae77849272861acaea
bluepome98 <wpdn1998@gmail.com>
2025-02-14 21:46:14 +0900

    🎉 add git pull로 dev 브랜치 끌어온 후 CreateFeed 컴포넌트 재생성

--- src/shared/NavigationLayout.jsx
+++ src/shared/NavigationLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import logo from '../assets/test-logo.png';
