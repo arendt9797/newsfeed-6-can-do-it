4fdea01c79ff1702a836974df42d468c224eac81
verdantgreeny <verdantgreeny@gmail.com>
2025-02-15 00:56:57 +0900

    💄style: text color 변경

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -63,6 +63,8 @@ const StCategoriesSection = styled.section`
     text-decoration: none;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
     transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    color: black;
+    padding: 10px;
 
     &:hover {
       box-shadow: 0 14px 28px rgba(0, 0, 0, 0.13),
