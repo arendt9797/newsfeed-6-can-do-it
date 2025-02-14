3825e66db1feb40c2b2b59598d434b6ab9e036bb
verdantgreeny <verdantgreeny@gmail.com>
2025-02-14 18:59:28 +0900

    ✨feat: div를 Link로 변경하여 홈으로 이동하게끔 변경

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 function Category() {
   return (
     <StCategoriesSection>
-      <div className="one">One</div>
-      <div className="two">Two</div>
-      <div className="three">Three</div>
-      <div className="four">Four</div>
-      <div className="five">Five</div>
-      <div className="six">Six</div>
-      <div className="seven">Seven</div>
-      <div className="eigth">Eight</div>
-      <div className="nine">Nine</div>
-      <div className="ten">Ten</div>
+      <Link to="/">One</Link>
+      <Link to="/" className="two">
+        Two
+      </Link>
+      <Link to="/" className="three">
+        Three
+      </Link>
+      <Link to="/" className="four">
+        Four
+      </Link>
+      <Link to="/" className="five">
+        Five
+      </Link>
+      <Link to="/" className="six">
+        Six
+      </Link>
+      <Link to="/">Seven</Link>
+      <Link to="/">Eight</Link>
+      <Link to="/">Nine</Link>
+      {/* 기타는 고정 */}
+      <Link to="/" className="ten">
+        기타
+      </Link>
     </StCategoriesSection>
   );
 }
@@ -26,12 +40,27 @@ const StCategoriesSection = styled.section`
   text-align: center;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-gap: 3px;
+  grid-gap: 5px;
   grid-auto-rows: minmax(100px, auto);
 
-  div {
-    border: solid 2px black;
+  a {
+    background-color: #bab7b72d;
     border-radius: 14px;
+    text-decoration: none;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+
+    &:hover {
+      box-shadow: 0 14px 28px rgba(0, 0, 0, 0.13),
+        0 10px 10px rgba(0, 0, 0, 0.11);
+      cursor: pointer;
+    }
+  }
+
+  .two,
+  .three,
+  .six {
+    background-color: #6764642c;
   }
 
   .two {
