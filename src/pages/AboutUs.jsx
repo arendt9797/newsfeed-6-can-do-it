bea6eb362e87d7c174fc687633d9032f570af5d4
bonseung-dev <glynnregister@gmail.com>
2025-02-15 13:29:37 +0900

    💄 Style:기존에 만들어 놓았던 기본UI 적용
    style:기존에 만들어 놓았던 기본UI 적용

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,14 @@
+// AboutUs.jsx
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
 import { supabase } from '../supabase/client';
 
 function AboutUs() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]); // 사용자 데이터 저장
 
   useEffect(() => {
     const fetchUsers = async () => {
+      // users 테이블 데이터 가져오기
       const { data, error } = await supabase.from('users').select('*');
       if (error) {
         console.error('사용자 정보 가져오기 오류:', error);
@@ -19,16 +21,76 @@ function AboutUs() {
   }, []);
 
   return (
-    <div>
-      <h1>About Our Project</h1>
-      <h2>Hi Developer!</h2>
-      {users && users.length > 0 ? (
-        users.map((user) => <div key={user.id}>{user.nickname}</div>)
-      ) : (
-        <p>등록된 사용자가 없습니다.</p>
-      )}
-    </div>
+    <StyledAboutUsContainer>
+      <div>
+        <h1>About Our Project</h1>
+      </div>
+      <div>
+        <h2>Hi Developer!</h2>
+        <StyledTeamMemberList>
+          {users && users.length > 0 ? (
+            users.map((user) => (
+              <StyledTeamMemberCard key={user.id}>
+                <StyledMemberPhoto
+                  src={user.my_profile_image_url}
+                  alt={user.nickname || 'User'}
+                />
+                <StyledMemberInfo>
+                  <h3>{user.nickname}</h3>
+                  {user.github && (
+                    <p>
+                      <a href={user.github} target="_blank">
+                        Github
+                      </a>
+                    </p>
+                  )}
+                  {user.blog && (
+                    <p>
+                      <a href={user.blog} target="_blank">
+                        Blog
+                      </a>
+                    </p>
+                  )}
+                </StyledMemberInfo>
+              </StyledTeamMemberCard>
+            ))
+          ) : (
+            <p>등록된 사용자가 없습니다.</p>
+          )}
+        </StyledTeamMemberList>
+      </div>
+    </StyledAboutUsContainer>
   );
 }
+
+const StyledAboutUsContainer = styled.div`
+  margin: 0 auto;
+`;
+
+const StyledTeamMemberList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`;
+
+const StyledTeamMemberCard = styled.li`
+  display: flex;
+  float: left;
+  align-items: center;
+  width: 500px;
+  height: 150px;
+  margin: 20px 90px;
+  border: 1px solid #ccc;
+  padding: 10px;
+`;
+
+const StyledMemberPhoto = styled.img`
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  margin-right: 20px;
+  margin-left: 5px;
+`;
+
+const StyledMemberInfo = styled.div``;
 
 export default AboutUs;
