3c298b2bf423ee9532d49113a2db92c8f2d7ae09
verdantgreeny <verdantgreeny@gmail.com>
2025-02-16 23:42:58 +0900

    ✨feat: 카테고리에 맞게 이미지 배치"

--- src/constants/categoryName.js
+++ src/constants/categoryName.js
@@ -8,3 +8,24 @@ export const GAMES = '게임';
 export const COOKING = '요리';
 export const ANIMALS = '동물';
 export const ETC = '기타';
+
+import movie from '../assets/movie.png';
+import travel from '../assets/travel.png';
+import art from '../assets/art.png';
+import music from '../assets/music.png';
+import diy from '../assets/diy.png';
+import exercise from '../assets/exercise.png';
+import game from '../assets/game.png';
+import cooking from '../assets/cooking.png';
+import animal from '../assets/animal.png';
+export const categoryImgArr = [
+  movie,
+  travel,
+  art,
+  music,
+  diy,
+  exercise,
+  game,
+  cooking,
+  animal,
+];
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,8 @@
 import { useContext } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import categories from '../constants/categories';
 import { AuthContext } from '../context/AuthProvider';
-import { ETC } from '../constants/categoryName';
+import * as c from '../constants/categoryName';
 import logo from '../assets/test-logo.png';
 import { useNavigate } from 'react-router-dom';
 
@@ -15,17 +14,13 @@ function Category() {
   const myInterests =
     user?.user_interests
       ?.map((i) => i.user_interest)
-      .sort((a, b) => (a === ETC ? 1 : b === ETC ? -1 : 0)) || [];
-
-  // console.log(myInterests);
+      .sort((a, b) => (a === c.ETC ? 1 : b === c.ETC ? -1 : 0)) || [];
 
   const testImg = user?.my_profile_image_url || logo;
 
-  console.log(testImg);
-
   //user의 관심사 및 기타(ETC)를 제외한 카테고리
   const others = categories.filter(
-    (i) => !myInterests.includes(i) && i !== ETC,
+    (i) => !myInterests.includes(i) && i !== c.ETC,
   );
 
   const myPickSet = new Set([1, 2, 5]);
@@ -49,10 +44,15 @@ function Category() {
           const candidate = myInterests.shift() || defaultCategory;
           // 기타(ETC)가 myInterests에 있으면 others 배열의 마지막 요소로 대체
           loginCategory =
-            candidate === ETC && others.length ? others.pop() : candidate;
+            candidate === c.ETC && others.length ? others.pop() : candidate;
         } else {
           loginCategory = others.shift() || defaultCategory;
         }
+        // 카테고리에 맞는 이미지 찾기
+        const categoryImage = categories.includes(loginCategory)
+          ? c.categoryImgArr[categories.indexOf(loginCategory)]
+          : logo;
+
         return (
           <StButton
             onClick={() => {
@@ -60,7 +60,7 @@ function Category() {
             }}
             key={i}
             className={classNames[i]}
-            $test={testImg}
+            $img={categoryImage}
           >
             <p>{isLogin ? loginCategory : defaultCategory}</p>
           </StButton>
@@ -72,9 +72,9 @@ function Category() {
           navigate(`/`);
         }}
         className="ten"
-        $test={testImg}
+        $img={testImg}
       >
-        <p>{ETC}</p>
+        <p>{c.ETC}</p>
       </StButton>
     </StCategoriesSection>
   );
@@ -98,7 +98,6 @@ const StCategoriesSection = styled.section`
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
     transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
     color: black;
-
 
     &:hover {
       box-shadow: 0 14px 28px rgba(0, 0, 0, 0.13),
@@ -150,7 +149,7 @@ const StButton = styled.button`
   z-index: 1;
 
   &::after {
-    background-image: ${(prop) => `url(${prop.$test})`};
+    background-image: ${(prop) => `url(${prop.$img})`};
     background-position: center;
     background-size: cover;
     border-radius: 14px;
