9d832a31374ec400bab1bca735db8c028322b058
verdantgreeny <verdantgreeny@gmail.com>
2025-02-16 23:03:55 +0900

    💄style: category link를 button으로 변경

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -5,39 +5,77 @@ import categories from '../constants/categories';
 import { AuthContext } from '../context/AuthProvider';
 import { ETC } from '../constants/categoryName';
 import logo from '../assets/test-logo.png';
+import { useNavigate } from 'react-router-dom';
 
 function Category() {
   const { isLogin, user } = useContext(AuthContext);
+  const navigate = useNavigate();
   //user의 관심사 카테고리 배열로 가져오기
-  const myInterests = user?.user_interests?.map((i) => i.user_interest) || [];
-  //user의 관심사 및 기타를 제외한 카테고리
+  //기타(ETC)가 배열 안에 있을 경우 배열의 가장 마지막에 위치시키기
+  const myInterests =
+    user?.user_interests
+      ?.map((i) => i.user_interest)
+      .sort((a, b) => (a === ETC ? 1 : b === ETC ? -1 : 0)) || [];
+
+  // console.log(myInterests);
+
+  const testImg = user?.my_profile_image_url || logo;
+
+  console.log(testImg);
+
+  //user의 관심사 및 기타(ETC)를 제외한 카테고리
   const others = categories.filter(
     (i) => !myInterests.includes(i) && i !== ETC,
   );
 
-  const categoriesData = [
-    { class: 'one', login: others[0], default: categories[0] },
-    { class: 'two', login: myInterests[0], default: categories[1] },
-    { class: 'three', login: myInterests[1], default: categories[2] },
-    { class: 'four', login: others[1], default: categories[3] },
-    { class: 'five', login: others[2], default: categories[4] },
-    { class: 'six', login: myInterests[2], default: categories[5] },
-    { class: 'seven', login: others[3], default: categories[6] },
-    { class: 'eight', login: others[4], default: categories[7] },
-    { class: 'nine', login: others[5], default: categories[8] },
+  const myPickSet = new Set([1, 2, 5]);
+  const classNames = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
   ];
 
   return (
     <StCategoriesSection>
-      {categoriesData.map((c, i) => (
-        <Link key={i} to="/" className={c.class}>
-          <p>{isLogin ? c.login : c.default}</p>
-        </Link>
-      ))}
-      {/* 기타는 고정 */}
-      <Link to="/" className="ten">
-        <p> {ETC} </p>
-      </Link>
+      {categories.slice(0, 9).map((defaultCategory, i) => {
+        let loginCategory;
+        if (myPickSet.has(i)) {
+          const candidate = myInterests.shift() || defaultCategory;
+          // 기타(ETC)가 myInterests에 있으면 others 배열의 마지막 요소로 대체
+          loginCategory =
+            candidate === ETC && others.length ? others.pop() : candidate;
+        } else {
+          loginCategory = others.shift() || defaultCategory;
+        }
+        return (
+          <StButton
+            onClick={() => {
+              navigate(`/`);
+            }}
+            key={i}
+            className={classNames[i]}
+            $test={testImg}
+          >
+            <p>{isLogin ? loginCategory : defaultCategory}</p>
+          </StButton>
+        );
+      })}
+      {/* 기타(ETC)는 고정 */}
+      <StButton
+        onClick={() => {
+          navigate(`/`);
+        }}
+        className="ten"
+        $test={testImg}
+      >
+        <p>{ETC}</p>
+      </StButton>
     </StCategoriesSection>
   );
 }
@@ -53,16 +91,14 @@ const StCategoriesSection = styled.section`
   grid-gap: 6px;
   grid-auto-rows: minmax(100px, auto);
 
-  a {
+  button {
     background-color: #bab7b72d;
-    /* background-image: url(${logo});
-    background-position: center;
-    background-size: cover; */
+    border: none;
     border-radius: 14px;
-    text-decoration: none;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
     transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
     color: black;
+
 
     &:hover {
       box-shadow: 0 14px 28px rgba(0, 0, 0, 0.13),
@@ -71,10 +107,8 @@ const StCategoriesSection = styled.section`
     }
 
     p {
-      font-size: 20px;
-      background-color: #46d7ab;
-      padding: 5px;
-      border-radius: 14px 14px 0 0;
+      font-size: 25px;
+      font-weight: bold;
     }
   }
 
@@ -108,5 +142,26 @@ const StCategoriesSection = styled.section`
   .ten {
     grid-column: 1/4;
     grid-row: 6;
+  }
+`;
+
+const StButton = styled.button`
+  position: relative;
+  z-index: 1;
+
+  &::after {
+    background-image: ${(prop) => `url(${prop.$test})`};
+    background-position: center;
+    background-size: cover;
+    border-radius: 14px;
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    position: absolute;
+    content: '';
+    top: 0;
+    left: 0;
+    z-index: -1;
+    opacity: 0.5;
   }
 `;
